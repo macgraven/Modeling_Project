23513fd3698966990a00620ec6f8954fe3e03564
Mac Graven <macgraven3@gmail.com>
2018-11-28 18:34:29 -0500

    Updated L-V with additional simulations

--- Lotka-Volterra.py
+++ Lotka-Volterra.py
@@ -26,10 +26,29 @@ def Lot_Vol_Sim(y,t0,b,a,e,s):
 times = range(0,100) #Passed as t0
 y0=[25,5] #Passed as H and P, respectively
 params = (0.5,0.02,0.1,0.2) #Passed as (b,a,e,s)
-
 #Running the simulation using scipy integrate
 sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
 #Creating a dataframe
 simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
 a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
 print(a)
+
+#Trial 2 - Doubling b, a, P; Halving e, s, H
+#Should run without major problems
+times = range(0,100)
+y0 = [12,10] #Passed as H and P, respectively
+params = (1,0.04,0.05,0.1) #Passed as (b,a,e,s)
+sim2 = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+simdf2=pd.DataFrame({"t":times,"H":sim2[:,0],"P":sim2[:,1]})
+b=ggplot(simdf2,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf2,aes(x="t",y="P"),color="red")+theme_classic()
+print(b)
+
+#Trial 3 - 5x b, a, P; 0.2x e,s,H
+#Should be observably different
+times = range(0,100)
+y0=[5,25]
+params=(2.5,0.1,0.02,0.04)
+sim3=spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+simdf3=pd.DataFrame({"t":times,"H":sim3[:,0],"P":sim3[:,1]})
+c=ggplot(simdf3,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf3,aes(x="t",y="P"),color="red")+theme_classic()
+print(c)
