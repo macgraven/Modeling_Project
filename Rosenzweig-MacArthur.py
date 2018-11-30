4c6095a1b31008d6b2a47db02e89d696f0f4fc0f
Elisabeth Kerns <ekerns@nd.edu>
2018-11-30 11:18:03 -0500

    Rosenzweig-MacArthur.py

--- Rosenzweig-MacArthur.py
+++ Rosenzweig-MacArthur.py
@@ -26,8 +26,9 @@ def Ros_Mac_Sim(y,t0,b,a,w,d,e,s):
 #Define parameters
 params = (0.8,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
 times = range(0,100)
+#b = prey birth rate 
+#a = predator attack rate 
 
-#b=prey birth rate 
 #Simulate using odeint
 y0 = [500,120] #Passed as H and P, respectively
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
@@ -39,19 +40,40 @@ print(a)
 
 #Simulation 2: double b (prey birth rate) 
 #See Simulation 1 for descritions of variables, etc. 
-params = (0.16,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
+params = (1.6,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
 times = range(0,100)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
-simdf = pandas.DataFarme({"t":times,"H":sim[:,0],"P":sim[:,1]})
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
 a = ggplot(simdf,aes(x="t",y="H"))+geom_line() + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
 print(a)
 
-#Simulation 3: half b (prey birth rate)
+#Simulation 3: halve b (prey birth rate)
+params = (0.4,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0=[500,120]
+sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line() + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
 
-#Simulation 4: 
+#Simulation 4: double a (predator attack rate)
+params = (0.8,0.002,5,400,0.07,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0=[500,120]
+sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line() + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
 
-#Simulation 5: 
+#Simulation 5: halve a (predator attack rate)
+params = (0.8,0.0005,5,400,0.07,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0=[500,120]
+sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line() + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
 
 #Simulation 6: 
 
