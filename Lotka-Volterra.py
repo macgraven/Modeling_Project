faa92e1126c8976db345b32a6ac906477f7346e3
Mac Graven <macgraven3@gmail.com>
2018-11-30 11:06:22 -0500

    Simulations for all parameters for L-V

--- Lotka-Volterra.py
+++ Lotka-Volterra.py
@@ -10,6 +10,14 @@ import pandas as pd
 import scipy
 import scipy.integrate as spint
 from plotnine import *
+import numpy as np
+
+#H - Herbivore Birth Rate
+#P - Predator Attack Rate
+#b - Prey Birth Rate
+#a - Predator Death Rate
+#e - Conversion efficiency of prey to predators
+#s - Predator Death Rate
 
 def Lot_Vol_Sim(y,t0,b,a,e,s):
     
@@ -21,9 +29,9 @@ def Lot_Vol_Sim(y,t0,b,a,e,s):
     
     return [dH_dt,dP_dt]
 
-#Trial One - Given Parameters
+#Trial 1 - Given Parameters
 #b = 0.5; a = 0.02; e = 0.1; s = 0.2; H0 = 25; P0 = 5
-times = range(0,100) #Passed as t0
+times = np.arange(0,100,0.1) #Passed as t0
 y0=[25,5] #Passed as H and P, respectively
 params = (0.5,0.02,0.1,0.2) #Passed as (b,a,e,s)
 #Running the simulation using scipy integrate
@@ -32,23 +40,143 @@ sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
 simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
 a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
 print(a)
+print("Original Parameters")
 
-#Trial 2 - Doubling b, a, P; Halving e, s, H
-#Should run without major problems
-times = range(0,100)
-y0 = [12,10] #Passed as H and P, respectively
-params = (1,0.04,0.05,0.1) #Passed as (b,a,e,s)
+#Trial 2 - Doubling P
+#Should run without major changes
+times = np.arange(0,100,0.1)
+y0 = [25,10] #Passed as H and P, respectively
+params = (0.5,0.02,0.1,0.2) #Passed as (b,a,e,s)
 sim2 = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
 simdf2=pd.DataFrame({"t":times,"H":sim2[:,0],"P":sim2[:,1]})
 b=ggplot(simdf2,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf2,aes(x="t",y="P"),color="red")+theme_classic()
 print(b)
+print("P Doubled")
 
-#Trial 3 - 5x b, a, P; 0.2x e,s,H
+#Trial 3 - .5x P
 #Should be observably different
-times = range(0,100)
-y0=[5,25]
-params=(2.5,0.1,0.02,0.04)
+times = np.arange(0,100,0.1)
+y0=[25,2.5]
+params=(0.5,0.02,0.1,0.2)
 sim3=spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
 simdf3=pd.DataFrame({"t":times,"H":sim3[:,0],"P":sim3[:,1]})
 c=ggplot(simdf3,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf3,aes(x="t",y="P"),color="red")+theme_classic()
 print(c)
+print("P Halved")
+
+
+#Trial 4 - Doubling H
+times = np.arange(0,100,0.1)
+y0 = [50,5] #Passed as H and P, respectively
+params = (0.5,0.02,0.1,0.2) #Passed as (b,a,e,s)
+sim2 = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+simdf2=pd.DataFrame({"t":times,"H":sim2[:,0],"P":sim2[:,1]})
+d=ggplot(simdf2,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf2,aes(x="t",y="P"),color="red")+theme_classic()
+print(d)
+print("H Doubled")
+
+#Trial 5 - Halving H
+times = np.arange(0,100,0.1)
+y0=[12.5,5] #Passed as H and P, respectively
+params = (0.5,0.02,0.1,0.2) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("H Halved")
+
+#Trial 6 - b * 2
+times = np.arange(0,100,0.1)
+y0 = [25,5] #Passed as H and P, respectively
+params = (1,0.02,0.1,0.2) #Passed as (b,a,e,s)
+sim2 = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+simdf2=pd.DataFrame({"t":times,"H":sim2[:,0],"P":sim2[:,1]})
+e=ggplot(simdf2,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf2,aes(x="t",y="P"),color="red")+theme_classic()
+print(e)
+print("b Doubled")
+
+#Trial 7 - b * .5
+times = np.arange(0,100,0.1)
+y0=[25,5] #Passed as H and P, respectively
+params = (0.25,0.02,0.1,0.2) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("b Halved")
+
+#Trial 8 - a * 2
+times = np.arange(0,100,0.1)
+y0=[25,5] #Passed as H and P, respectively
+params = (0.5,0.04,0.1,0.2) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("a Doubled")
+
+#Trial 9 - a * .5
+times = np.arange(0,100,0.1)
+y0=[25,5] #Passed as H and P, respectively
+params = (0.5,0.01,0.1,0.2) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("a Halved")
+
+#Trial 10 - e * 2
+times = np.arange(0,100,0.1)
+y0=[25,5] #Passed as H and P, respectively
+params = (0.5,0.02,0.2,0.2) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("e Doubled")
+
+#Trail 11 - e * .5
+times = np.arange(0,100,0.1)
+y0=[25,5] #Passed as H and P, respectively
+params = (0.5,0.02,0.05,0.2) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("e Halved")
+
+#Trial 12 - s * 2
+times = np.arange(0,100,0.1)
+y0=[25,5] #Passed as H and P, respectively
+params = (0.5,0.02,0.1,0.4) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("s Doubled")
+
+#Trial 13 - s * .5
+times = np.arange(0,100,0.1)
+y0=[25,5] #Passed as H and P, respectively
+params = (0.5,0.02,0.1,0.1) #Passed as (b,a,e,s)
+#Running the simulation using scipy integrate
+sim = spint.odeint(func=Lot_Vol_Sim,y0=y0,t=times,args=params)
+#Creating a dataframe
+simdf=pd.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a=ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
+print("s Halved")
