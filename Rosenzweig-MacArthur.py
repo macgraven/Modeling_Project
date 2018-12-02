84af585952d601ebf66cb659acc7b59abfbeffb3
Elisabeth Kerns <ekerns@nd.edu>
2018-12-02 17:14:37 -0500

    Rosenzweig-MacArthur.py

--- Rosenzweig-MacArthur.py
+++ Rosenzweig-MacArthur.py
@@ -38,6 +38,7 @@ simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
 a = ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
 print(a)
 
+
 #Simulation 2: double b (prey birth rate) 
 #See Simulation 1 for descritions of variables, etc. 
 params = (1.6,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
@@ -75,8 +76,54 @@ simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
 a = ggplot(simdf,aes(x="t",y="H"))+geom_line() + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
 print(a)
 
-#Simulation 6: 
+#Simulation 6: double w
+params = (0.8,0.001,10,400,0.07,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0=[500,120]
+sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line() + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red")+theme_classic()
+print(a)
 
-#Simulation 7:
+#Simulation 7: halve w
+params = (0.8,0.001,2.5,400,0.07,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0 = [500,120]
+sim = spint.odeint(fun=Ros_Mac_Sim,y0=y0,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P")),color="red")+theme_classic()
+print(a)
 
-#Simulation 8: '
+#Simulation 8: double d
+params = (0.8,0.001,10,800,0.07,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0=[500,120]
+sim = spint.odeint(fun=Ros_Mac_Sim,y0=y0,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geom_line(simdf,aes(x="t",y="P")),color="red"+theme_classic()
+print(a)
+
+#Simulation 9: halve d
+params = (0.8,0.001,10,200,0.07,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0=[500,120]
+sim = spint.odeint(fun=Ros_Mac_Sim,y0=y0,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line()+ylab("Population")+geome_line(simdf,aes(x="t",y="P")),color="red"+theme_classic()
+print(a)
+
+#Simulation 10: double e
+params = (0.8,0.001,10,400,0.14,0.2) #(b,a,w,d,e,s)
+times = range(0,100)
+y0 = [500,120]
+sim = spint.odeint(fun=Ros_Mac_Sim,y0=yo,t=times,args=params)
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})o
+
+
+#Simulation 11: halve e
+params = (0.8,0.001,10,400,0.035,0.2) #(b,a,w,d,e,s)
+
+
+#Simulation 12: double s
+
+#Simulation 13: halve s 
