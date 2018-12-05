580ddb814908b88303d99236cb562b02751372df
Elisabeth Kerns <ekerns@nd.edu>
2018-12-05 16:55:40 -0500

    Rosenzweig-MacArthur.py

--- Rosenzweig-MacArthur.py
+++ Rosenzweig-MacArthur.py
@@ -10,7 +10,7 @@ import pandas
 import scipy
 import scipy.integrate as spint
 from plotnine import *
-import numpy as np 
+import numpy 
 
 #Custom function for Ros_Mac
 def Ros_Mac_Sim(y,t0,b,a,w,d,e,s):
@@ -27,9 +27,9 @@ def Ros_Mac_Sim(y,t0,b,a,w,d,e,s):
 #Simulation 1: standard parameters 
 #Define parameters
 params = (0.8,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 #b = prey birth rate 
-#a = alpha = intra-specfici competition coefficient = carrying capacity 
+#a = alpha = intra-specfic competition coefficient = carrying capacity 
 #w = maximum predator attack rate 
 #d = handling time of predator per prey = how much time predator spends on single prey (catch, digest, etc.)
 #e = conversion efficiency of prey to predators 
@@ -41,140 +41,143 @@ sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 
 #Creating a DataFrame
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="red",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
 
 #Simulation 2: double b (prey birth rate) 
 #See Simulation 1 for descritions of variables, etc. 
 params = (1.6,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="green",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
 #Simulation 3: halve b (prey birth rate)
 params = (0.4,0.001,5,400,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="green",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 4: triple a (predator attack rate)
+#Simulation 4: triple a (competition coefficient)
 params = (0.8,0.002,5,400,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
 a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 5: halve a (predator attack rate)
+#Simulation 5: halve a (competition coefficient)
 params = (0.8,0.0005,5,400,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
 a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 6: double w
+#Simulation 6: double w (maximum predator attack rate)
 params = (0.8,0.001,10,400,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="pink",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5) + ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 7: halve w
-params = (0.8,0.001,2.5,400,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
-y0 = [500,120]
-sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
-simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="pink",size=1.5)+theme_classic()
-print(a)
-
-#Simulation 8: double d
+#Simulation 8: double d (handling time predator per prey)
 params = (0.8,0.001,10,800,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="orange",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 9: halve d
+#Simulation 9: halve d (handling time predator per prey)
 params = (0.8,0.001,10,200,0.07,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0=[500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black")+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="orange",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 10: double e
+#Simulation 10: double e (conversion efficiency)
 params = (0.8,0.001,10,400,0.14,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="purple",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
 
-#Simulation 11: halve e
+#Simulation 11: halve e (conversion efficiency)
 params = (0.8,0.001,10,400,0.035,0.2) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="purple",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 12: triple s
+#Simulation 12: triple s (predator death rate)
 params = (0.8,0.001,5,400,0.07,0.6) #(b,a,w,d,e,s)
-times = np.arrage(0,100,0.1)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="magenta",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
-#Simulation 13: halve s 
-params = (0.8,0.001,5,400,0.07,0.1) #(b,a,w,d,e,s)\
-times = np.arrage(0,100,0.1)
+#Simulation 13: halve s (predator death rate)
+params = (0.8,0.001,5,400,0.07,0.1) #(b,a,w,d,e,s)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
-a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="magenta",size=1.5)+theme_classic()
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
 print(a)
 
 
 #Paradox of Enrichment 
 #Simulation 14:
-times = np.arrange(0,100,0.1)
+params = (0.8,0.00125,5,400,0.07,0.1) #(b,a,w,d,e,s)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
 simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
+print(a)
 
 #Simulation 15: 
-times = np.arrange(0,100,0.1)
+params = (0.8,0.001,5,400,0.07,0.1) #(b,a,w,d,e,s)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
-simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})od
+simdf = pandas.DataFrame({"t":times,"H":sim[:,0],"P":sim[:,1]})
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
+print(a)
 
 #Simulation 16:
-times = np.arrange(0,100,0.1)
+params = (0.8,0.00075,5,400,0.07,0.1) #(b,a,w,d,e,s)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
+print(a)
 
 #Simulation 17: 
-times = np.arrange(0,100,0.1)
+params = (0.8,0.0005,5,400,0.07,0.1) #(b,a,w,d,e,s)
+times = numpy.arange(0,100,0.1)
 y0 = [500,120]
 sim = spint.odeint(func=Ros_Mac_Sim,y0=y0,t=times,args=params)
+a = ggplot(simdf,aes(x="t",y="H"))+geom_line(color="black",size=1.5)+ylab("Population")+geom_line(simdf,aes(x="t",y="P"),color="blue",size=1.5)+theme_classic()
+print(a)
